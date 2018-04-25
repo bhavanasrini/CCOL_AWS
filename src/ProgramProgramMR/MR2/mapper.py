46a22a2ab797d8f0abe23c6471fc297eecb1f3fe
Bhavana Srinivasan <bhavanaaiyer@gmail.com>
2018-04-24 21:40:29 -0600

    MR 2nd phase

---
+++ src/ProgramProgramMR/MR2/mapper.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import sys
+import json
+import os
+import ast
+for line in sys.stdin:
+	#fileName = os.environ['map_input_file']
+	#tag = int(fileName[-5:-4])
+	line = line.strip()
+	my_dict = json.loads(line)
+	my_dict = ast.literal_eval(my_dict)
+	my_dict['tag']=2
+	print "%s" %(my_dict)
---
+++ src/ProgramProgramMR/MR2/reducer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+import os
+import sys
+
+for line in sys.stdin:
+	line=line.strip()
+	print (line)
